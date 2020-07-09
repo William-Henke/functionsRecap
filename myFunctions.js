605a47dfaaa39081493a705288572f82a6a6c534
unknown <william.henke@computingforall.org>
2020-07-09 13:12:51 -0700

    This is the backbone code for this project.

---
+++ myFunctions.js
@@ -0,0 +1,10 @@
+let pokeArray = [
+    {name: 'Bulbasaur', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'},
+    {name: 'Ivysaur', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'},
+    {name: 'Charmander', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'},
+    {name: 'Charizard', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'},
+    {name: 'Squirtle', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'},
+  
+  ];
+
+  
