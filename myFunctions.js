763e4b35367c23a23d98a3e45caed75cd6f44f40
unknown <william.henke@computingforall.org>
2020-07-09 16:16:27 -0700

    Added some more pokemon to the array.

--- myFunctions.js
+++ myFunctions.js
@@ -1,10 +1,19 @@
 let pokeArray = [
     {name: 'Bulbasaur', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'},
     {name: 'Ivysaur', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'},
+    {name: 'Venusaur', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'},
     {name: 'Charmander', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'},
+    {name: 'Charmeleon', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'},
     {name: 'Charizard', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'},
     {name: 'Squirtle', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'},
-  
+    {name: 'Wartortle', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png'},
+    {name: 'Blastoise', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png'},
+    {name: 'Caterpie', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png'},
+    {name: 'Metapod', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png'},
+    {name: 'Butterfree', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png'},
+    {name: 'Weedle', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png'},
+    {name: 'kakuna', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png'},
+    {name: 'Beedrill', image:'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png'},
   ];
 
 let button = document.getElementById('pokeButton');
@@ -59,10 +68,10 @@ function pokeCarousel2() {
     pokePic2.src =  pokeArray[index2].image;
 }
 
-function showingPoke() {
-    // console.log('I am working');
-    document.getElementById('firstPokeName').style.display = "block";
-    document.getElementById('firstPokePic').style.display = "block";
-    document.getElementById('secondPokeName').style.display = "block";
-    document.getElementById('secondPokePic').style.display = "block";    
-}
+// function showingPoke() {
+//     // console.log('I am working');
+//     document.getElementById('firstPokeName').style.display = "block";
+//     document.getElementById('firstPokePic').style.display = "block";
+//     document.getElementById('secondPokeName').style.display = "block";
+//     document.getElementById('secondPokePic').style.display = "block";    
+// }
