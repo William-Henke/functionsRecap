a0e3d6279c92e92c5714cdb34f773f24fd18faa7
unknown <william.henke@computingforall.org>
2020-07-09 13:41:12 -0700

    initial button styling added.

--- myFunctions.js
+++ myFunctions.js
@@ -10,20 +10,30 @@ let pokeArray = [
 let button = document.getElementById('pokeButton');
 button.addEventListener('click', pokeCarousel);
 
+var index1 = 0;
+var index2 = -1;
+
 function pokeCarousel() {
-    if() {
-        // function to make the array loop.
+    if(index1 >= pokeArray.length -1) {
+        index1 = 0;        
+    }
+
+    if(index2 >= pokeArray.length -1) {
+        index2 = 0;        
     }
 
     let pokeName1 = document.getElementById('firstPokeName');
-    pokeName1.innerHTML = 
+    pokeName1.innerHTML = pokeArray[index1].name;
 
     let pokePic1 = document.getElementById('firstPokePic');
-    pokePic1.src = 
+    pokePic1.src = pokeArray[index1].image;
 
     let pokeName2 = document.getElementById('secondPokeName');
-    pokeName2.innerHTML = 
+    pokeName2.innerHTML = pokeArray[index2].name;
 
     let pokePic2 = document.getElementById('secondPokePic');
-    pokePic2.src =     
+    pokePic2.src =  pokeArray[index2].image;
+    
+    index1++;
+    index2++;
 }
